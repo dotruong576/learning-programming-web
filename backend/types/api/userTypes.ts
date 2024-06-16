31e48b80839f40df9ed803635fb9c22e44d7c66d
Dino200503 <dotruong576@gmail.com>
2024-06-17 00:11:46 +0700

    add userTypes

---
+++ backend/types/api/userTypes.ts
@@ -0,0 +1,13 @@
+import { UserSchema } from '../schema/userSchemaType';
+
+export type TGetUserDetailById = {
+  userId: string;
+};
+
+export type TGetUserDetailByEmail = {
+  email: string;
+};
+export type TUserPayload = Pick<UserSchema, 'email' | 'password' | 'fullName' | 'avatar'>;
+export type TUpdateProfileUser = Partial<Omit<TUserPayload, 'email'>>;
+
+export type TGetUserDetailDataResponse = Omit<UserSchema, 'password'>;
