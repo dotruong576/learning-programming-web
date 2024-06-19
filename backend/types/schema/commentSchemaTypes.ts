bc9eb8a58b33ec663cbd94a0d28aa06e68b4f4de
Dino200503 <dotruong576@gmail.com>
2024-06-19 16:50:47 +0700

    fix frontend-user

---
+++ backend/types/schema/commentSchemaTypes.ts
@@ -0,0 +1,10 @@
+export type TCommentSchema = {
+    userId: string;
+    content: string;
+    rating?: number;
+    likedUsers: string[];
+    unlikedUsers: string[];
+    replies: string[];
+    isReply: boolean;
+  };
+  
