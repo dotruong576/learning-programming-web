c73b6ccc2d7078058f45ac25cf97576b0c98c59b
Dino200503 <dotruong576@gmail.com>
2024-07-03 17:12:54 +0700

    connect firebase fireStore and init file route

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
-import authenticateMiddleware from '../middleware/auth';
+import authenticateMiddleware, { nonStrictAuthenticateMiddleware } from '../middleware/auth';
 import authRoute from './auth/route';
 import userRoute from './user/route';
 import courseRoute from './course/route';
+import userCourseRoute from './userCourses/route';
+import fileRoute from './file/route';
 const apiRoute = express.Router();
 
 apiRoute.use('/auth', authRoute);
 apiRoute.use('/user', authenticateMiddleware, userRoute);
 apiRoute.use('/course',courseRoute);
+apiRoute.use('/user-courses',nonStrictAuthenticateMiddleware, userCourseRoute);
+apiRoute.use('/file',authenticateMiddleware, fileRoute);
+
 
 export default apiRoute;
