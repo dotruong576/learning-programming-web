bbb62e99ce15f84556d018dbec2213f34a2d875e
Dino200503 <dotruong576@gmail.com>
2024-07-04 18:33:42 +0700

    init api lesson

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -5,6 +5,7 @@ import userRoute from './user/route';
 import courseRoute from './course/route';
 import userCourseRoute from './userCourses/route';
 import fileRoute from './file/route';
+import lessonRoute from './lesson/route';
 const apiRoute = express.Router();
 
 apiRoute.use('/auth', authRoute);
@@ -12,6 +13,7 @@ apiRoute.use('/user', authenticateMiddleware, userRoute);
 apiRoute.use('/course',courseRoute);
 apiRoute.use('/user-courses',nonStrictAuthenticateMiddleware, userCourseRoute);
 apiRoute.use('/file',authenticateMiddleware, fileRoute);
+apiRoute.use('/lesson',authenticateMiddleware, lessonRoute);
 
 
 export default apiRoute;
