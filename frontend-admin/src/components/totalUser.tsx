c6a4e3c419c70b55f2829a8c834956cf61ce9aa3
Bao <21520604@gm.uit.edu.vn>
2024-07-09 14:42:00 +0700

    add missing file for frontend admin

---
+++ frontend-admin/src/components/totalUser.tsx
@@ -0,0 +1,8 @@
+export default function UserCount() {
+  return (
+    <div className={"h-1/2 p-8 shadow-2xl"}>
+      <p className={"text-4xl font-bold"}>Total Users</p>
+      <p className={"text-2xl"}>100000000</p>
+    </div>
+  );
+}
