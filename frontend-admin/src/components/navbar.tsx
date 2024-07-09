5d8a2c9d04b8a8003bb909878dee29db9fa7638d
Dino200503 <dotruong576@gmail.com>
2024-07-10 04:30:56 +0700

    fix auth

--- frontend-admin/src/components/navbar.tsx
+++ frontend-admin/src/components/navbar.tsx
@@ -1,22 +1,13 @@
 "use client";
 import React, { useContext } from "react";
-import { redirect, useRouter } from "next/navigation";
-import { userContext } from "~/context/UserContext";
 import AvatarMenu from "~/components/AvatarMenu";
 
 export default function Navbar() {
-  const router = useRouter();
-  const { isLogin: auth, data } = useContext(userContext);
 
   return (
     <nav className="flex h-10vh w-full items-center justify-end space-x-8 bg-main-color">
       <div>
-        {auth ? (
           <AvatarMenu />
-        ) : (
-          redirect('/403')
-        )
-        }
       </div>
     </nav>
   );
